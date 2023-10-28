d063c5437648639d1d3cab73dc5ea727ea5c9a39
Devin Elrose <devin@twentyideas.com>
2023-10-28 15:25:39 -0700

    adds a Card component

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+export const Card = styled.div(({ theme }) => ({
+  backgroundColor: theme.color.background.section,
+  border: `1px solid ${theme.color.stroke}`,
+  borderRadius: theme.cornerRadius,
+  padding: theme.spacing(2),
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  gap: theme.spacing(1.5),
+}));
---
+++ src/theme/emotion.d.ts
@@ -0,0 +1,6 @@
+import '@emotion/react'
+import type { Theme as CustomTheme } from "./theme.ts"
+
+declare module '@emotion/react' {
+  export interface Theme extends CustomTheme {}
+}
