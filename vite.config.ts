ba334856bfcfa7520a5683ba20f374aaff508e82
Devin Elrose <devin@twentyideas.com>
2023-10-28 19:24:29 -0700

    sets the base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Unfonts from "unplugin-fonts/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL ?? "/", // for GitHub Pages
   plugins: [
     react(),
     Unfonts({
