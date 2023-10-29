c1981d1414ce3fc791ff0e1ee227e1d65771db95
Devin Elrose <devin@twentyideas.com>
2023-10-28 19:26:15 -0700

    resolve vite warning message "(!) "base" option should start with a slash."

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import Unfonts from "unplugin-fonts/vite"
 
+const baseUrl = process.env.BASE_URL ?? "/"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL ?? "/", // for GitHub Pages
+  base: baseUrl.startsWith("/") ? baseUrl : `/${baseUrl}`, // for GitHub Pages
   plugins: [
     react(),
     Unfonts({
